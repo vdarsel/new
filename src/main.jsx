d7dd4ce50fbf2e9e672b73221b38dcbf28421ec7
Vianey Darsel <vdarsel@live.fr>
2023-02-17 16:27:46 +0100

    root

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,9 @@ import App from './App';
 import './index.css';
 import { HashRouter as Router } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.createRoot(document.getElementById('root')).render(
   <Router>
     <App />
   </Router>
