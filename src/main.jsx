df452318d766b54a244e838fffe2d4adbd0f6e34
Vianey Darsel <vdarsel@live.fr>
2023-02-17 17:14:42 +0100

    main

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { BrowserRouter, createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, createBrowserRouter, RouterProvider} from "react-router-dom";
 import App from './App';
 import './index.css';
-import { HashRouter as Router } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.createRoot(document.getElementById('root')).render(
-  <Router>
-    <App />
-  </Router>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
