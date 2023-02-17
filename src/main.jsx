aa9f11f44ff92256f0978ecf0cf6ff4847f0ba1d
Vianey Darsel <vdarsel@live.fr>
2023-02-17 16:23:20 +0100

    Change

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, createBrowserRouter, RouterProvider} from "react-router-dom";
+// import { BrowserRouter, createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from './App';
 import './index.css';
-
+import { HashRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <Router>
+    <App />
+  </Router>
 );
